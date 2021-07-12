eed031c7f51b3010740d58e5ee82c6912a7649ae
abaialihan <abai.alihan@gmail.com>
2021-07-13 01:39:18 +0600

    add online system

--- SuberDriver/app/src/main/java/com/example/suberdriver/Common.kt
+++ SuberDriver/app/src/main/java/com/example/suberdriver/Common.kt
@@ -3,6 +3,7 @@ package com.example.suberdriver
 import com.example.suberdriver.model.DriverInfoModel
 
 object Common {
+    const val DRIVER_LOCATION_REFERENCE: String = "DriverLocation"
     const val DRIVER_INFO_REFERENCE: String = "DriverInfo"
     var currentUser: DriverInfoModel? = null
 }
--- SuberDriver/app/src/main/java/com/example/suberdriver/activities/ui/home/HomeFragment.kt
+++ SuberDriver/app/src/main/java/com/example/suberdriver/activities/ui/home/HomeFragment.kt
@@ -13,7 +13,10 @@ import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.suberdriver.Common
 import com.example.suberdriver.R
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,6 +24,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -30,6 +36,9 @@ import com.karumi.dexter.listener.single.PermissionListener
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var mapFragment: SupportMapFragment
+
     //GoogleMap
     private lateinit var mMap: GoogleMap
 
@@ -38,8 +47,22 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var locationCallback: LocationCallback
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    private lateinit var homeViewModel: HomeViewModel
-    private lateinit var mapFragment: SupportMapFragment
+    //User online system
+    private lateinit var geoFire: GeoFire
+    private lateinit var onlineRef: DatabaseReference
+    private lateinit var currentUserRef: DatabaseReference
+    private lateinit var driverLocationRef: DatabaseReference
+
+    private val onlineValueEventListener = object: ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            if (snapshot.exists())
+                currentUserRef.onDisconnect().removeValue()
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Snackbar.make(mapFragment.requireView(), error.message, Snackbar.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -56,38 +79,22 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         return root
     }
 
-    override fun onDestroy() {
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-        super.onDestroy()
+    override fun onResume() {
+        super.onResume()
+        registerOnlineSystem()
     }
 
-    //Initialize location
-    @SuppressLint("MissingPermission")
-    private fun initLocation() {
-        locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.fastestInterval = 3000
-        locationRequest.interval = 5000
-        locationRequest.smallestDisplacement = 10f
-
-        locationCallback = object: LocationCallback(){
-            override fun onLocationResult(locationResult: LocationResult) {
-                super.onLocationResult(locationResult)
-
-                val newPos = LatLng(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos, 18f))
-            }
-        }
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper())
-
+    override fun onDestroy() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        geoFire.removeLocation(FirebaseAuth.getInstance().currentUser!!.uid)
+        onlineRef.removeEventListener(onlineValueEventListener)
+        super.onDestroy()
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
 
-        Dexter.withContext(context)
+        Dexter.withContext(requireContext())
             .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             .withListener(object: PermissionListener{
 
@@ -107,6 +114,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                         true
                     }
 
+                    //Add location button
                     val view = mapFragment.requireView().findViewById<View>("1".toInt()).parent as View
                     val locationButton = view.findViewById<View>("2".toInt())
                     val params = locationButton.layoutParams as RelativeLayout.LayoutParams
@@ -127,11 +135,64 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             }).check()
 
         try {
-            val success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.uber_maps_style))
+            val success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.uber_maps_style))
             if (!success)
                 Log.e("TAG_ERROR", "Map parsing error")
         }catch (e: Resources.NotFoundException){
             e.printStackTrace()
         }
     }
+
+    //Initialize online system
+    private fun initOnlineSystem(){
+        onlineRef = FirebaseDatabase.getInstance().reference.child(".info/connected")
+        driverLocationRef = FirebaseDatabase.getInstance().getReference(Common.DRIVER_LOCATION_REFERENCE)
+        currentUserRef = FirebaseDatabase.getInstance().getReference(Common.DRIVER_LOCATION_REFERENCE).child(
+            FirebaseAuth.getInstance().currentUser!!.uid
+        )
+        geoFire = GeoFire(driverLocationRef)
+    }
+
+    //Initialize location
+    @SuppressLint("MissingPermission")
+    private fun initLocation() {
+        initOnlineSystem()
+        registerOnlineSystem()
+
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.fastestInterval = 3000
+        locationRequest.interval = 5000
+        locationRequest.smallestDisplacement = 10f
+
+        locationCallback = object: LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                val newPos = LatLng(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos, 18f))
+
+                //Update location
+                geoFire.setLocation(
+                    FirebaseAuth.getInstance().currentUser!!.uid,
+                    GeoLocation(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
+                ){ key: String?, error: DatabaseError? ->
+                    if (error != null)
+                        Snackbar.make(mapFragment.requireView(), error.message, Snackbar.LENGTH_LONG).show()
+                    else
+                        Snackbar.make(mapFragment.requireView(), "You are online", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+    }
+
+    private fun registerOnlineSystem() {
+        onlineRef.addValueEventListener(onlineValueEventListener)
+    }
+
+
 }
