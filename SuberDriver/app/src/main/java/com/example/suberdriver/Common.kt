6070116f98e7416419e6f33d6b8e0e2e63125910
abaialihan <abai.alihan@gmail.com>
2021-07-15 04:45:30 +0600

    configure and pars driver info panel

--- SuberDriver/app/src/main/java/com/example/suberdriver/Common.kt
+++ SuberDriver/app/src/main/java/com/example/suberdriver/Common.kt
@@ -1,6 +1,7 @@
 package com.example.suberdriver
 
 import com.example.suberdriver.model.DriverInfoModel
+import java.lang.StringBuilder
 
 object Common {
     const val DRIVER_LOCATION_REFERENCE: String = "DriverLocation"
--- SuberDriver/app/src/main/java/com/example/suberdriver/activities/DriverHomeActivity.kt
+++ SuberDriver/app/src/main/java/com/example/suberdriver/activities/DriverHomeActivity.kt
@@ -1,7 +1,12 @@
 package com.example.suberdriver.activities
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,13 +16,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import com.example.suberdriver.Common
 import com.example.suberdriver.R
 import com.example.suberdriver.databinding.ActivityDriverHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DriverHomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityDriverHomeBinding
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,18 +42,60 @@ class DriverHomeActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_driver_home)
+
+        drawerLayout = binding.drawerLayout
+        navView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment_content_driver_home)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        buildAlertDialog()
+    }
+
+    private fun buildAlertDialog(){
+        navView.setNavigationItemSelectedListener {
+            if (it.itemId == R.id.nav_sign_out){
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.menu_sign_out)
+                    .setMessage(R.string.do_really_exit)
+                    .setNegativeButton(R.string.cancel) {dialog, wich ->
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton(R.string.menu_sign_out) {dialog, wich ->
+                        FirebaseAuth.getInstance().signOut()
+                        val intent = Intent(this, SplashScreenActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        finish()
+                    }.setCancelable(false)
+
+                val dialog = builder.create()
+                dialog.setOnShowListener {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                        .setTextColor(resources.getColor(R.color.black))
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                        .setTextColor(resources.getColor(R.color.black))
+                }
+
+                dialog.show()
+            }
+            true
+        }
+        initializeDriverInfo()
+    }
+
+    private fun initializeDriverInfo(){
+        val headerView = navView.getHeaderView(0)
+        val txtName = headerView.findViewById<View>(R.id.txt_full_name) as TextView
+        val txtPhone = headerView.findViewById<View>(R.id.txt_phone) as TextView
+        val txtRate = headerView.findViewById<View>(R.id.txt_rate) as TextView
+
+        txtName.text = Common.currentUser!!.firstName + " " + Common.currentUser!!.lastName
+        txtPhone.text = Common.currentUser!!.phoneNumber
+        txtRate.text = Common.currentUser!!.rating.toString()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
